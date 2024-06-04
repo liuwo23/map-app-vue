37b2c36c85a6ee780d1920a011c532d1f40705a5
YuliiaDikun <dikun.yulia@gmail.com>
2024-06-04 13:12:52 +0300

    update tailwind config to ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
 export default {
   content: [
     "./index.html",
@@ -15,5 +16,5 @@ export default {
     },
   },
   plugins: [],
-}
+} satisfies Config
 
