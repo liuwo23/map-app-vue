969370a857c39eaa485d4a220057761e4704a11a
liuwo23 <37184494+liuwo23@users.noreply.github.com>
2024-09-02 16:03:37 +0800

    Update GreetingView.vue

--- src/views/GreetingView.vue
+++ src/views/GreetingView.vue
@@ -4,10 +4,9 @@
       <img class="inline mb-6" src="../assets/img/map-pin.svg" alt="" />
       <h1 class="font-bold text-4xl mb-7">IT traveler</h1>
       <p class="leading-6 mb-11 max-w-xl mx-auto">
-        Простий і зручний веб додаток, який дозволить тобі відмічати твої улюблені місця, а також
-        ті, в яких би ти дуже хотів побувати. Тож не зволікай і спробуй сам.
+        简单易用的网页应用程序，让你可以标记你喜欢的场所，以及你非常想去的地方。不要犹豫，现在就试试吧。
       </p>
-      <IButton :to="'/auth/registration'" variant="primary">Почати роботу</IButton>
+      <IButton :to="'/auth/registration'" variant="primary">开始工作</IButton>
     </div>
   </BaseLayout>
 </template>
